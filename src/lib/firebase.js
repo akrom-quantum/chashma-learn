5f003e79a042e917c3504a19b801c7ee35b27f96
akrom-quantum <abdinabiyevakrom@yu.ac.kr>
2026-04-01 12:57:54 +0900

    Update firebase.js

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -1,6 +1,6 @@
 "use client";
 import { initializeApp, getApps } from "firebase/app";
-import { getAuth } from "firebase/auth";
+import { getAuth, browserLocalPersistence, setPersistence } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
@@ -13,7 +13,11 @@ const firebaseConfig = {
 };
 
 const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0];
-
 export const auth = getAuth(app);
 export const db   = getFirestore(app);
+
+if (typeof window !== "undefined") {
+  setPersistence(auth, browserLocalPersistence);
+}
+
 export default app;
