772ad9d79b2a5cd7137dc04f831a9bd78be2a4d5
akrom-quantum <abdinabiyevakrom@yu.ac.kr>
2026-04-01 12:48:42 +0900

    Create firebase.js

---
+++ src/lib/firebase.js
@@ -0,0 +1,19 @@
+"use client";
+import { initializeApp, getApps } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey:            "AIzaSyBD65CTP7Tx84l-qL-KT9pj3uMUOsLOCI4",
+  authDomain:        "chashma-learn.firebaseapp.com",
+  projectId:         "chashma-learn",
+  storageBucket:     "chashma-learn.firebasestorage.app",
+  messagingSenderId: "1059701555295",
+  appId:             "1:1059701555295:web:104a64e41d60252a28dbea",
+};
+
+const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0];
+
+export const auth = getAuth(app);
+export const db   = getFirestore(app);
+export default app;
