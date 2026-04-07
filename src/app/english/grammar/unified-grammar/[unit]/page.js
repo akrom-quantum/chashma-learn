60f0665f30db9029fe1fc1602d2fa1c5514ef3c0
akrom-quantum <abdinabiyevakrom@yu.ac.kr>
2026-04-07 15:43:11 +0900

    Create page.js

---
+++ src/app/english/grammar/unified-grammar/[unit]/page.js
@@ -0,0 +1,13 @@
+import UnitClient from "./UnitClient";
+
+export default async function UnitPage({ params }) {
+  const { subject, book, unit } = await params;
+
+  return (
+    <UnitClient
+      subject={subject}
+      book={book}
+      unitId={unit}
+    />
+  );
+}
