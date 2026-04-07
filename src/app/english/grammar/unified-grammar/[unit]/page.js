1e3453c1fb5b0730ae727c97b57606394ee43e53
akrom-quantum <abdinabiyevakrom@yu.ac.kr>
2026-04-07 18:30:21 +0900

    Update page.js

--- src/app/english/grammar/unified-grammar/[unit]/page.js
+++ src/app/english/grammar/unified-grammar/[unit]/page.js
@@ -2,12 +2,5 @@ import UnitClient from "./UnitClient";
 
 export default async function UnitPage({ params }) {
   const { subject, book, unit } = await params;
-
-  return (
-    <UnitClient
-      subject={subject}
-      book={book}
-      unitId={unit}
-    />
-  );
+  return <UnitClient subject={subject} book={book} unitId={unit} />;
 }
