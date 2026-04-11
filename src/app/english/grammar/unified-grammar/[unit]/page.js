fa82b5a2a4a7e375fb5099116d4ac66c254f51c5
akrom-quantum <abdinabiyevakrom@yu.ac.kr>
2026-04-11 15:15:03 +0900

    Refactor UnitPage to use static subject and bookId

--- src/app/english/grammar/unified-grammar/[unit]/page.js
+++ src/app/english/grammar/unified-grammar/[unit]/page.js
@@ -3,12 +3,12 @@ import UnitClient from "./UnitClient";
 export const dynamic = "force-dynamic";
 
 export default async function UnitPage({ params }) {
-  const { subject, book, unit } = await params;
+  const { unit } = await params;
 
   return (
     <UnitClient
-      subject={subject}
-      bookId={book}
+      subject="grammar"
+      bookId="unified-grammar"
       unitId={unit}
     />
   );
