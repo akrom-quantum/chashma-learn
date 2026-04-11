0ef147d3293d15f98027f35374a6aedc95d6e4e0
akrom-quantum <abdinabiyevakrom@yu.ac.kr>
2026-04-11 10:50:48 +0900

    Add Link import to login page

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -1,5 +1,6 @@
 "use client";
 
+import Link from "next/link";
 import { useState, useEffect } from "react";
 import { auth, db } from "@/lib/firebase";
 import { onAuthStateChanged, browserLocalPersistence, setPersistence } from "firebase/auth";
