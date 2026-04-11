1fbfc07daa3abcf237753c05f9f68277af25fd93
akrom-quantum <abdinabiyevakrom@yu.ac.kr>
2026-04-11 11:11:18 +0900

    Update page.js

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -1,6 +1,6 @@
 "use client";
 import Link from "next/link";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useRouter } from "next/navigation";
 import { useAuth } from "@/lib/AuthContext";
 
