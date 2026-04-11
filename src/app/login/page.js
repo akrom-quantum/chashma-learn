ff9e11ef1275d44b9c9de376c7fab3e90f076faa
akrom-quantum <abdinabiyevakrom@yu.ac.kr>
2026-04-11 10:52:54 +0900

    Update page.js

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -1,6 +1,5 @@
 "use client";
 
-import Link from "next/link";
 import { useState, useEffect } from "react";
 import { auth, db } from "@/lib/firebase";
 import { onAuthStateChanged, browserLocalPersistence, setPersistence } from "firebase/auth";
